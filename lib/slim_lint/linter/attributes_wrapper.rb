7a0d40fdb10311455ce7068d5063528b1fd62998
Rob Cameron <cannikinn@gmail.com>
2018-09-14 08:52:35 -0700

    Adds AttributesWrapper linter

---
+++ lib/slim_lint/linter/attributes_wrapper.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+module SlimLint
+  # Checks for missing or superfluous spacing before and after control statements.
+  class Linter::AttributesWrapper < Linter
+    include LinterRegistry
+
+    STYLES = {
+      :round  => ['\(', '\)'],
+      :curly  => ['\{', '\}'],
+      :square => ['\[', '\]'],
+      :none   => [' ', ''],
+      :any    => ['[\(\{\[]', '[\)\}\]]']
+    }
+    WRAPPER_START = '\A\s*[\w\.#-]+'
+    WRAPPER_ANY = '.*'
+    WRAPPER_END = '.*\z'
+    ATTRIBUTES_REGEX = Regexp.new(WRAPPER_START + '[ \(\{\[]')
+
+    on_start do |_sexp|
+      style = config.fetch('style', 'none').to_sym
+      dummy_node = Struct.new(:line)
+
+      document.source_lines.each_with_index do |line, index|
+        # whether the line contains any attributes at all
+        next unless line =~ ATTRIBUTES_REGEX
+        next if line =~ line_regex(style)
+
+        report_lint(dummy_node.new(index + 1), message(style))
+      end
+    end
+
+    private
+
+    def line_regex(style)
+      @line_regex ||= Regexp.new(
+        WRAPPER_START +
+        STYLES[style].first +
+        WRAPPER_ANY +
+        STYLES[style].last +
+        WRAPPER_END
+      )
+    end
+
+    def message(style)
+      message = 'Should '
+      message + case style
+                when :none
+                  'not have any attribute wrapper characters'
+                when :any
+                  'wrap attributes in either (), [] or {}'
+                else
+                  "wrap attributes in #{STYLES[style].first}#{STYLES[style].last}"
+                end
+    end
+
+  end
+end
---
+++ spec/slim_lint/linter/attributes_wrapper_spec.rb
@@ -0,0 +1,716 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe SlimLint::Linter::AttributesWrapper do
+  include_context 'linter'
+
+  context 'element with attributes' do
+
+    context "when config style is 'none'" do
+
+      let(:config) do
+        SlimLint::ConfigurationLoader.load_hash({
+          'linters' => {
+            'AttributesWrapper' => {
+              'enabled' => true,
+              'style' => 'none'
+            }
+          }
+        }).for_linter(described_class)
+      end
+
+      context "with no attribute wrapper" do
+
+        context "with HTML only" do
+
+          context 'attribute with no value' do
+            let(:slim) { 'div data-foo' }
+
+            it { should_not report_lint }
+          end
+
+          context 'tag with class shortcut' do
+            let(:slim) { 'div.foo data-foo="bar"' }
+
+            it { should_not report_lint }
+          end
+
+          context 'tag with id shortcut' do
+            let(:slim) { 'div#foo data-foo="bar"' }
+
+            it { should_not report_lint }
+          end
+
+          context 'tag with id and class shortcut' do
+            let(:slim) { 'div#foo.bar data-foo="bar"' }
+
+            it { should_not report_lint }
+          end
+
+          context 'tag with numbers' do
+            let(:slim) { 'h1 data-foo="bar"' }
+
+            it { should_not report_lint }
+          end
+
+          context 'attribute single quoted value' do
+            let(:slim) { "div data-foo='bar'" }
+
+            it { should_not report_lint }
+          end
+
+          context 'leading whitespace' do
+            let(:slim) { '  div data-foo="bar"' }
+
+            it { should_not report_lint }
+          end
+
+          context 'with text' do
+            let(:slim) { 'div data-foo="bar" Foobar' }
+
+            it { should_not report_lint }
+          end
+
+          context 'with output' do
+
+            context 'with leading whitespace' do
+              let(:slim) { 'div data-foo="bar" = "Foobar"' }
+
+              it { should_not report_lint }
+            end
+
+            context 'without leading whitespace' do
+              let(:slim) { 'div data-foo="bar"= "Foobar"' }
+
+              it { should_not report_lint }
+            end
+
+          end
+
+          context "with interpolation inside attribute values" do
+
+            context "ruby-style in attribute value" do
+              let(:slim) { 'div data-foo="#{bar}"= "Foobar"' }
+
+              it { should_not report_lint }
+            end
+
+            context "method call as attribute value" do
+              let(:slim) { 'div data-foo=bar= "Foobar"' }
+
+              it { should_not report_lint }
+            end
+
+          end
+
+        end
+
+      end
+
+      context "with round attribute wrapper" do
+
+        context "with HTML only" do
+
+          context 'attribute with no value' do
+            let(:slim) { 'div(data-foo)' }
+
+            it { should report_lint }
+          end
+
+          context 'tag with class shortcut' do
+            let(:slim) { 'div.foo(data-foo="bar")' }
+
+            it { should report_lint }
+          end
+
+          context 'tag with id shortcut' do
+            let(:slim) { 'div#foo(data-foo="bar")' }
+
+            it { should report_lint }
+          end
+
+          context 'tag with id and class shortcut' do
+            let(:slim) { 'div#foo.bar(data-foo="bar")' }
+
+            it { should report_lint }
+          end
+
+          context 'tag with numbers' do
+            let(:slim) { 'h1(data-foo="bar")' }
+
+            it { should report_lint }
+          end
+
+          context 'attribute with value, double quotes' do
+            let(:slim) { 'div(data-foo="bar")' }
+
+            it { should report_lint }
+          end
+
+          context 'attribute with value, single quotes' do
+            let(:slim) { "div(data-foo='bar')" }
+
+            it { should report_lint }
+          end
+
+          context 'leading whitespace' do
+            let(:slim) { '  div(data-foo="bar")' }
+
+            it { should report_lint }
+          end
+
+          context 'with text' do
+            let(:slim) { 'div(data-foo="bar") Foobar' }
+
+            it { should report_lint }
+          end
+
+        end
+
+        context 'with output' do
+
+          context 'with leading whitespace' do
+            let(:slim) { 'div(data-foo="bar") = "Foobar"' }
+
+            it { should report_lint }
+          end
+
+          context 'without leading whitespace' do
+            let(:slim) { 'div(data-foo="bar")= "Foobar"' }
+
+            it { should report_lint }
+          end
+
+        end
+
+        context "with interpolation inside attribute values" do
+
+          context "ruby-style in attribute value" do
+            let(:slim) { 'div(data-foo="#{bar}")= "Foobar"' }
+
+            it { should report_lint }
+          end
+
+          context "method call as attribute value" do
+            let(:slim) { 'div(data-foo=bar)= "Foobar"' }
+
+            it { should report_lint }
+          end
+
+        end
+
+      end
+
+      context "with square attribute wrapper" do
+
+        context "with HTML only" do
+
+          context 'attribute with no value' do
+            let(:slim) { 'div[data-foo]' }
+
+            it { should report_lint }
+          end
+
+        end
+
+      end
+
+      context "with curly attribute wrapper" do
+
+        context "with HTML only" do
+
+          context 'attribute with no value' do
+            let(:slim) { 'div{data-foo}' }
+
+            it { should report_lint }
+          end
+
+        end
+
+        context "with interpolation inside attribute values" do
+
+          context "ruby-style in attribute value" do
+            let(:slim) { 'div{data-foo="#{bar}"}= "Foobar"' }
+
+            it { should report_lint }
+          end
+
+        end
+
+      end
+
+    end
+
+    context "when config style is 'round'" do
+
+      let(:config) do
+        SlimLint::ConfigurationLoader.load_hash({
+          'linters' => {
+            'AttributesWrapper' => {
+              'enabled' => true,
+              'style' => 'round'
+            }
+          }
+        }).for_linter(described_class)
+      end
+
+      context "with no attribute wrapper" do
+
+        context "with HTML only" do
+
+          context 'attribute with no value' do
+            let(:slim) { 'div data-foo' }
+
+            it { should report_lint }
+          end
+
+          context 'tag with class shortcut' do
+            let(:slim) { 'div.foo data-foo="bar"' }
+
+            it { should report_lint }
+          end
+
+          context 'tag with id shortcut' do
+            let(:slim) { 'div#foo data-foo="bar"' }
+
+            it { should report_lint }
+          end
+
+          context 'tag with id and class shortcut' do
+            let(:slim) { 'div#foo.bar data-foo="bar"' }
+
+            it { should report_lint }
+          end
+
+          context 'tag with numbers' do
+            let(:slim) { 'h1 data-foo="bar"' }
+
+            it { should report_lint }
+          end
+
+          context 'attribute single quoted value' do
+            let(:slim) { "div data-foo='bar'" }
+
+            it { should report_lint }
+          end
+
+          context 'leading whitespace' do
+            let(:slim) { '  div data-foo="bar"' }
+
+            it { should report_lint }
+          end
+
+          context 'with text' do
+            let(:slim) { 'div data-foo="bar" Foobar' }
+
+            it { should report_lint }
+          end
+
+        end
+
+        context 'with output' do
+
+          context 'with leading whitespace' do
+            let(:slim) { 'div data-foo="bar" = "Foobar"' }
+
+            it { should report_lint }
+          end
+
+          context 'without leading whitespace' do
+            let(:slim) { 'div data-foo="bar"= "Foobar"' }
+
+            it { should report_lint }
+          end
+
+        end
+
+        context "with interpolation inside attribute values" do
+
+          context "ruby-style in attribute value" do
+            let(:slim) { 'div data-foo="#{bar}"= "Foobar"' }
+
+            it { should report_lint }
+          end
+
+          context "method call as attribute value" do
+            let(:slim) { 'div data-foo=bar= "Foobar"' }
+
+            it { should report_lint }
+          end
+
+        end
+
+      end
+
+      context "with round attribute wrapper" do
+
+        context "with HTML only" do
+
+          context 'attribute with no value' do
+            let(:slim) { 'div(data-foo)' }
+
+            it { should_not report_lint }
+          end
+
+          context 'tag with class shortcut' do
+            let(:slim) { 'div.foo(data-foo="bar")' }
+
+            it { should_not report_lint }
+          end
+
+          context 'tag with id shortcut' do
+            let(:slim) { 'div#foo(data-foo="bar")' }
+
+            it { should_not report_lint }
+          end
+
+          context 'tag with id and class shortcut' do
+            let(:slim) { 'div#foo.bar(data-foo="bar")' }
+
+            it { should_not report_lint }
+          end
+
+          context 'tag with numbers' do
+            let(:slim) { 'h1(data-foo="bar")' }
+
+            it { should_not report_lint }
+          end
+
+          context 'attribute with value, double quotes' do
+            let(:slim) { 'div(data-foo="bar")' }
+
+            it { should_not report_lint }
+          end
+
+          context 'attribute with value, single quotes' do
+            let(:slim) { "div(data-foo='bar')" }
+
+            it { should_not report_lint }
+          end
+
+          context 'leading whitespace' do
+            let(:slim) { '  div(data-foo="bar")' }
+
+            it { should_not report_lint }
+          end
+
+          context 'with text' do
+            let(:slim) { 'div(data-foo="bar") Foobar' }
+
+            it { should_not report_lint }
+          end
+
+        end
+
+        context 'with output' do
+
+          context 'with leading whitespace' do
+            let(:slim) { 'div(data-foo="bar") = "Foobar"' }
+
+            it { should_not report_lint }
+          end
+
+          context 'without leading whitespace' do
+            let(:slim) { 'div(data-foo="bar")= "Foobar"' }
+
+            it { should_not report_lint }
+          end
+
+        end
+
+        context "with interpolation inside attribute values" do
+
+          context "ruby-style in attribute value" do
+            let(:slim) { 'div(data-foo="#{bar}")= "Foobar"' }
+
+            it { should_not report_lint }
+          end
+
+          context "method call as attribute value" do
+            let(:slim) { 'div(data-foo=bar)= "Foobar"' }
+
+            it { should_not report_lint }
+          end
+
+        end
+
+      end
+
+    end
+
+    context "when config style is 'square'" do
+
+      let(:config) do
+        SlimLint::ConfigurationLoader.load_hash({
+          'linters' => {
+            'AttributesWrapper' => {
+              'enabled' => true,
+              'style' => 'square'
+            }
+          }
+        }).for_linter(described_class)
+      end
+
+      context "with no attribute wrapper" do
+
+        context "with HTML only" do
+
+          context 'attribute with no value' do
+            let(:slim) { 'div data-foo' }
+
+            it { should report_lint }
+          end
+
+        end
+
+      end
+
+      context "with round attribute wrapper" do
+
+        context "with HTML only" do
+
+          context 'attribute with no value' do
+            let(:slim) { 'div(data-foo)' }
+
+            it { should report_lint }
+          end
+
+        end
+
+      end
+
+      context "with square attribute wrapper" do
+
+        context "with HTML only" do
+
+          context 'attribute with no value' do
+            let(:slim) { 'div[data-foo]' }
+
+            it { should_not report_lint }
+          end
+
+        end
+
+        context "with interpolation inside attribute values" do
+
+          context "ruby-style in attribute value" do
+            let(:slim) { 'div[data-foo="#{bar}"]= "Foobar"' }
+
+            it { should_not report_lint }
+          end
+
+        end
+
+      end
+
+      context "with curly attribute wrapper" do
+
+        context "with HTML only" do
+
+          context 'attribute with no value' do
+            let(:slim) { 'div{data-foo}' }
+
+            it { should report_lint }
+          end
+
+        end
+
+        context "with interpolation inside attribute values" do
+
+          context "ruby-style in attribute value" do
+            let(:slim) { 'div{data-foo="#{bar}"}= "Foobar"' }
+
+            it { should report_lint }
+          end
+
+        end
+
+      end
+
+    end
+
+    context "when config style is 'curly'" do
+
+      let(:config) do
+        SlimLint::ConfigurationLoader.load_hash({
+          'linters' => {
+            'AttributesWrapper' => {
+              'enabled' => true,
+              'style' => 'curly'
+            }
+          }
+        }).for_linter(described_class)
+      end
+
+      context "with no attribute wrapper" do
+
+        context "with HTML only" do
+
+          context 'attribute with no value' do
+            let(:slim) { 'div data-foo' }
+
+            it { should report_lint }
+          end
+
+        end
+
+        context "with interpolation inside attribute values" do
+
+          context "ruby-style in attribute value" do
+            let(:slim) { 'div data-foo="#{bar}" = "Foobar"' }
+
+            it { should report_lint }
+          end
+
+        end
+
+      end
+
+      context "with round attribute wrapper" do
+
+        context "with HTML only" do
+
+          context 'attribute with no value' do
+            let(:slim) { 'div(data-foo)' }
+
+            it { should report_lint }
+          end
+
+        end
+
+        context "with interpolation inside attribute values" do
+
+          context "ruby-style in attribute value" do
+            let(:slim) { 'div(data-foo="#{bar}")= "Foobar"' }
+
+            it { should report_lint }
+          end
+
+        end
+
+      end
+
+      context "with square attribute wrapper" do
+
+        context "with HTML only" do
+
+          context 'attribute with no value' do
+            let(:slim) { 'div[data-foo]' }
+
+            it { should report_lint }
+          end
+
+        end
+
+        context "with interpolation inside attribute values" do
+
+          context "ruby-style in attribute value" do
+            let(:slim) { 'div[data-foo="#{bar}"]= "Foobar"' }
+
+            it { should report_lint }
+          end
+
+        end
+
+      end
+
+      context "with curly attribute wrapper" do
+
+        context "with HTML only" do
+
+          context 'attribute with no value' do
+            let(:slim) { 'div{data-foo}' }
+
+            it { should_not report_lint }
+          end
+
+        end
+
+        context "with interpolation inside attribute values" do
+
+          context "ruby-style in attribute value" do
+            let(:slim) { 'div{data-foo="#{bar}"}= "Foobar"' }
+
+            it { should_not report_lint }
+          end
+
+        end
+
+      end
+
+    end
+
+  end
+
+  context 'element without attributes' do
+
+    context 'element only' do
+      let(:slim) { 'div' }
+
+      it { should_not report_lint }
+    end
+
+    context 'element with a class shortcut' do
+      let(:slim) { '  div.foo' }
+
+      it { should_not report_lint }
+    end
+
+    context 'element with an id shortcut' do
+      let(:slim) { 'div#bar' }
+
+      it { should_not report_lint }
+    end
+
+    context 'element with both class and id shortcuts' do
+      let(:slim) { '  div#foo.bar' }
+
+      it { should_not report_lint }
+    end
+
+    context 'only class shortcut' do
+      let(:slim) { '.foo' }
+
+      it { should_not report_lint }
+    end
+
+    context 'only id shortcut' do
+      let(:slim) { '  #bar' }
+
+      it { should_not report_lint }
+    end
+
+    context 'id and class shortcut' do
+      let(:slim) { '#bar.foo' }
+
+      it { should_not report_lint }
+    end
+
+    context 'multiple class shortcuts' do
+      let(:slim) { '.foo.bar' }
+
+      it { should_not report_lint }
+    end
+
+    context 'control code' do
+      let(:slim) { '- if foobar' }
+
+      it { should_not report_lint }
+    end
+
+    context 'output only' do
+      let(:slim) { '= "Foobar"' }
+
+      it { should_not report_lint }
+    end
+
+    context 'output after element' do
+      let(:slim) { 'div.foo= "Foobar"' }
+
+      it { should_not report_lint }
+    end
+
+  end
+
+end
